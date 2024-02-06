b639a3f69cab1d053b94d73c90afef6f463b9f1a
shakyapeiris <shakyaimanjith32@gmail.com>
2024-02-06 22:21:50 +0530

    Fix syntax in format.sh

--- format.sh
+++ format.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -e Ganison_dataset_1.csv] && [ -e Ganison_dataset_2.csv] && [ -e ganison_dataset_3.csv]&& [ -e ganison_dataset_4.csv]&& [ -e ganison_dataset_5.csv]
+if [ -e Ganison_dataset_1.csv ] && [ -e Ganison_dataset_2.csv ] && [ -e ganison_dataset_3.csv ] && [ -e ganison_dataset_4.csv ] && [ -e ganison_dataset_5.csv ]
 then
     python DataFromatting.py
     python manage.py loaddata area award class school student subject
